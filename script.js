a86556fa04bce835ce56f53dda4e990943198a42
Bünyamin Ilhan <ilhan.buenyain@gmail..com>
2023-05-22 19:08:02 +0200

    bug fixes

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function renderPokemon(responsAsJson, index, url) {
   let content = document.getElementById('content');
 
   if (responsAsJson) {
-    const sprite = responsAsJson['sprites']['other']['official-artwork']['front_default'];
+    const sprite = responsAsJson['sprites']['front_default'];
     const name = responsAsJson['name'];
     const type = responsAsJson['types'][0]['type']['name'];
     let type2 = '';
@@ -72,13 +72,16 @@ function generateStyle(index, type) {
 }
 
 function dNone(index) {
+
+  if (null) {
+    document.getElementById(`search-result`).innerHTML = `big-img${index}`
+  }
   document.getElementById(`big-img${index}`).classList.add('d-add');
 }
 
 function dNoneRemove(index) {
   document.getElementById(`big-img${index}`).classList.remove('d-add');
 }
-
 function showSmall(event, index) {
   let div = document.getElementById(`big-img${index}`);
   if (event.target.id !== `big-img${index}`) {
@@ -87,61 +90,13 @@ function showSmall(event, index) {
   div.classList.remove('d-add');
 }
 
-//--------------------------------------Search----------------------------------------------------- 
-
-async function createSearchArray() {
-  let searchResults = document.getElementById('search-result');
-  searchResults.innerHTML = '';
-let foundPokemons = [];                                           //gefundene werden hier gelagert um doppelt zu vermeiden
-
-  for (let i = 1; i < 1011; i++) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
-    let response = await fetch(url);
-    response = await response.json();
-    searchPokemon(response,searchResults,foundPokemons,i);
-  }
-}
-
-
-
-function searchPokemon(response,searchResults,foundPokemons,index){
-  let name = response['name'];
-  let picture = response['sprites']['front_shiny'];
-  let type = response['types'][0]['type']['name'];
-
-  let search = document.getElementById('search').value;
-  search = search.toLowerCase();
-
-  if (name.toLowerCase().includes(search) && !foundPokemons.includes(name)) {
-    foundPokemons.push(name);
-
-document.getElementById('found-info').innerHTML = 'gefunden'
-
-    searchResults.innerHTML += `
-    <div onclick="dNone(${index})" id="card${index}" class="card" style="width: 15rem;">
-    <span class="id"><b># ${index}</b></span>
-    <h5 class="card-title">${name}</h5>
-  <div class="img-container">
-  <div class="card-body">
-  <p id="card-text${index}" class="card-text">${type}</p>
-
-</div>
-    <img class="pokemon-img" src="${picture}" class="card-img-top" alt="...">
-  </div>  
-
-</div>
-`; 
-generateStyle(index, type)
-  }
-}
-
 
 //----------------------------------------Pokemon Galerie-----------------------------------------------
 
-function showPokeCard(index, sprite, name, type, type2, responsAsJson, url) {
+function showPokeCard(index, sprite2, name, type, type2, responsAsJson) {
   let primaryColor = colors[type] ? colors[type]['primaryColor'] : '';
   let secondColor = colors[type] ? colors[type]['secondColor'] : '';
-
+  let sprite = responsAsJson['sprites']['other']['official-artwork']['front_default']
   return `
       <div onclick="showSmall(event, ${index})" id="big-img${index}" class="big-img-container">
         <img class="arrow-left" onclick="previousPokemon(${index})" src="img/arrow-left.png">
@@ -183,4 +138,53 @@ function previousPokemon(index) {
     index = end;
     dNone(index);
   }
+}
+
+
+//--------------------------------------Search----------------------------------------------------- 
+
+async function createSearchArray() {
+  let searchResults = document.getElementById('search-result');
+  searchResults.innerHTML = '';
+  let foundPokemons = [];                                           //gefundene werden hier gelagert um doppelt zu vermeiden
+
+  for (let i = 1; i < 1011; i++) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
+    let response = await fetch(url);
+    response = await response.json();
+    searchPokemon(response, searchResults, foundPokemons, i);
+  }
+}
+
+
+
+function searchPokemon(response, searchResults, foundPokemons, index) {
+  let name = response['name'];
+  let picture = response['sprites']['front_shiny'];
+  let type = response['types'][0]['type']['name'];
+
+  let search = document.getElementById('search').value;
+  search = search.toLowerCase();
+
+  if (name.toLowerCase().includes(search) && !foundPokemons.includes(name)) {
+    foundPokemons.push(name);
+    if (search.length > 0) {
+      document.getElementById('found-info').innerHTML = `Es gab ${foundPokemons.length} treffer`
+    }
+    searchResults.innerHTML += `
+    <div onclick="dNone(${index})" id="card${index}" class="card" style="width: 15rem;">
+    <span class="id"><b># ${index}</b></span>
+    <h5 class="card-title">${name}</h5>
+  <div class="img-container">
+  <div class="card-body">
+  <p id="card-text${index}" class="card-text">${type}</p>
+</div>
+    <img class="pokemon-img" src="${picture}" class="card-img-top" alt="...">
+  </div>  
+
+</div>
+`;
+    generateStyle(index, type)
+  }
+  console.log(foundPokemons.length)
 }
